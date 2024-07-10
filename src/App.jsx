3cb0b0f611515db0b7908004562041740babf9d7
kavita chauthani <kckabu81@gmail.com>
2024-07-10 11:06:46 +0530

    frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
-import { Reposit } from "./pages/Reposit";
+
 const App = () => {
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/reposit" element={<Reposit />} />
         </Routes>
       </BrowserRouter>
     </>
